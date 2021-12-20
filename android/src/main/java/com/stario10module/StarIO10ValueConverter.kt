cab1f184e3d67aa2d066abea334072ada5a01230
Harmit Mendapara <59364073+harmit001@users.noreply.github.com>
2021-12-20 07:00:37 -0500

    Update StarIO10ValueConverter.kt

--- android/src/main/java/com/stario10module/StarIO10ValueConverter.kt
+++ android/src/main/java/com/stario10module/StarIO10ValueConverter.kt
@@ -728,9 +728,9 @@ class StarIO10ValueConverter {
                 val client = OkHttpClient()
                 val request = Request.Builder().url(uri).build()
                 val response = client.newCall(request).execute()
-                response.body?.bytes()?.let { responseBytes ->
+                response.body()?.bytes()?.let { responseBytes ->
                     bytes = responseBytes.toList()
-                    response.body?.close()
+                    response.body()?.close()
                 }
             } catch (e: Exception){}
 
